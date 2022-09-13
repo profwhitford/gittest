6a7b3ae1778db32833f4b4e47918cac48c6f5d4b
Paul <p.whitford@northeastern.edu>
2022-09-13 16:32:43 -0400

    added student placeholders

--- src/classprogram.py
+++ src/classprogram.py
@@ -1,2 +1,93 @@
 #!/opt/local/bin/python
-Let's make a python program together.
+#Let's make a python program together.
+
+# student 1 
+
+
+# student 2 
+
+
+# student 3 
+
+
+# student 4 
+
+
+# student 5 
+
+
+# student 6 
+
+
+# student 7 
+
+
+# student 8 
+
+
+# student 9 
+
+
+# student 10 
+
+
+# student 11 
+
+
+# student 12 
+
+
+# student 13 
+
+
+# student 14 
+
+
+# student 15 
+
+
+# student 16 
+
+
+# student 17 
+
+
+# student 18 
+
+
+# student 19 
+
+
+# student 20 
+
+
+# student 21 
+
+
+# student 22 
+
+
+# student 23 
+
+
+# student 24 
+
+
+# student 25 
+
+
+# student 26 
+
+
+# student 27 
+
+
+# student 28 
+
+
+# student 29 
+
+
+# student 30 
+
+
